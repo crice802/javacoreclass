55890f250bab0deb296ce4ee1f33990d76d3fd43
crice802 <rcrice@gmail.com>
2022-04-28 09:55:11 -0400

    getter & setter complete

---
+++ src/com/perscholas/cafe/Product.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.perscholas.cafe;
+
+/**
+ * @author crice
+ *
+ */
+public class Product {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
