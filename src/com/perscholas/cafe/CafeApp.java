b371500773ae73252234e5a8549493b7bb12bc3f
crice802 <rcrice@gmail.com>
2022-05-05 18:01:34 -0400

    remove files in gitignore

--- src/com/perscholas/cafe/CafeApp.java
+++ src/com/perscholas/cafe/CafeApp.java
@@ -3,6 +3,9 @@
  */
 package com.perscholas.cafe;
 
+import java.io.IOException;
+import java.util.Scanner;
+
 /**
  * @author crice
  *
@@ -14,6 +17,23 @@ public class CafeApp {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Product coffee = new Product();
+		Product espresso = new Product();
+		Product cappuccino = new Product();
+
+		cappuccino.setPrice(3.00);
+		espresso.setPrice(2.00);
+		coffee.setPrice(1.00);
+		cappuccino.setDescription("Lots of foam.");
+		espresso.setDescription("Bold, black and string");
+		coffee.setDescription("Boiled bean juice.");
+
+		try (Scanner input = new Scanner(System.in);) {
+			String order = input.next();
+			input.nextLine();
+		} catch (IOException e) {
+			e.getStackTrace();
+		}
 
 	}
 
--- src/com/perscholas/cafe/Product.java
+++ src/com/perscholas/cafe/Product.java
@@ -9,11 +9,52 @@ package com.perscholas.cafe;
  */
 public class Product {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	private double price;
+	private int quantity;
+	private String name, description;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	public double calculateProductTotal(double price) {
+		double subTotal = price * quantity;
+		return subTotal;
+	}
+
+	public Product() {
+
+	}
+
+	public Product(String name, String description, double price) {
 
 	}
 
