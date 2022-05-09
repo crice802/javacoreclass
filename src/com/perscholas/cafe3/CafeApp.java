2bb52c7eec355b9965f1c9b50e6a18593a28dfa1
crice802 <rcrice@gmail.com>
2022-05-09 19:24:20 -0400

    Stup up cafe project 3

---
+++ src/com/perscholas/cafe3/CafeApp.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.perscholas.cafe3;
+
+/**
+ * @author crice
+ *
+ */
+public class CafeApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/com/perscholas/cafe3/Coffee.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package com.perscholas.cafe3;
+
+/**
+ * @author crice
+ *
+ */
+public class Coffee {
+
+}
